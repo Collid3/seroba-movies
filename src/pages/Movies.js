1823bf8f7a99d7fa80a784375cb9aeab9c473ef4
Collid3 <masolathabangjerry11@gmail.com>
2023-08-28 13:07:45 +0200

    empty list when search is not found fix

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,29 +1,34 @@
 import "../styles/movies.css";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import DataContext from "../context/DataContext";
 import MovieCard from "../components/MovieCard";
 
 const Movies = () => {
   const { movies, API_IMG, search } = useContext(DataContext);
-  const [category, setCategory] = useState("popular");
 
   return (
     <div className="movies-container">
       <ul className="movies">
-        {movies
-          .filter((movie) =>
-            movie.title.toLowerCase().includes(search.toLowerCase())
-          )
-          .map((movie) => {
-            return (
-              <MovieCard
-                key={movie.id}
-                movie={movie}
-                image_api={API_IMG}
-                search={search}
-              />
-            );
-          })}
+        {movies.filter((movie) =>
+          movie.title.toLowerCase().includes(search.toLowerCase())
+        ).length > 0 ? (
+          movies
+            .filter((movie) =>
+              movie.title.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((movie) => {
+              return (
+                <MovieCard
+                  key={movie.id}
+                  movie={movie}
+                  image_api={API_IMG}
+                  search={search}
+                />
+              );
+            })
+        ) : (
+          <p>Search for "{search}" not found</p>
+        )}
       </ul>
     </div>
   );
